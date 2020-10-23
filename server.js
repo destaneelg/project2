de8dc78d25495cdc610f7a84ddb0acf6ecdef38d
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-22 20:30:04 -0500

    New server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 var mysql = require("mysql");
 var express = require("express");
 var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 8080;
+var app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
