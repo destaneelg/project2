cc43bb6392c1157f7790fceaada4f394947ca455
Greg Malone <atx76dragon@yahoo.com>
2020-10-29 16:30:15 -0500

    Simplified schema to create an empty database, since the sequelize will set up and structure the database instead

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,4 @@
-drop database if exists projects_db;
-create database projects_db;
-use database projects_db;
-
-create table projects(
-    id int not null auto_increment primary key,
-    ProjectName varchar(50),
-    Scope varchar(250),
-);
-
-table projectOwner(
-    id int not null auto_increment primary key,
-    ProjectOwner varchar(30),
-    Urgency varchar(15)
-);
-
-table projectPriority(
-    id int not null auto_increment primary key,
-    ExpectedCompletionDate date 0000-00-00,
-    Status varchar(15)
-    -- ProjectDurationDays int (4) not null
-);
+-- Drops the projects database if it exists currently --
+DROP DATABASE IF EXISTS projects_db;
+-- Creates the "projects" database --
+CREATE DATABASE projects_db;
