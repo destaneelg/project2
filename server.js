f7b88f66093bf88c6373cfdffde4bdba0d7f33d2
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-29 19:30:17 -0500

    updating jawsdb code

--- server.js
+++ server.js
@@ -16,6 +16,32 @@ app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
+{
+  "development": {
+    "username": "root",
+    "password": "Bthootu16",
+    "database": "blogger",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  }
+}
 
 
 // Routes
