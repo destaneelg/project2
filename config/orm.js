c57fac1e3de88ce271d13e89138ddde5aa350d6d
Destanee Glenn <destanee.glenn@gmail.com>
2020-10-30 14:56:52 -0500

    connecting app

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,5 @@
-var connection = require("./connection.js");
 
 // Object Relational Mapper (ORM)
-
 // The ?? signs are for swapping out table or column names
 // The ? signs are for swapping out other values
 // These help avoid SQL injection
