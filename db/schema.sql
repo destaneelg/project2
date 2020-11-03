05eba2f2b2f52e6921a7cbee6deab20ac2d870c5
Emily Aguirre <emgal.aguirre@gmail.com>
2020-11-02 21:55:42 -0500

    corrected bugs

--- db/schema.sql
+++
@@ -1,4 +0,0 @@
--- Drops the projects database if it exists currently --
-DROP DATABASE IF EXISTS projects_db;
--- Creates the "projects" database --
-CREATE DATABASE projects_db;
--- server.js
+++ server.js
@@ -1,54 +1,29 @@
-// Starting point for our Node/Express server
-var path = require("path");
-// Dependencies
+// *** Dependencies
+// =============================================================
 var express = require("express");
 
-// Setting up Express App 
+// Sets up the Express App
+// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Models for syncing
+// Requiring our models for syncing
 var db = require("./models");
-app.use(express.static(path.join(__dirname, "/public/assets")));
-// Code that sets up Express app to handle data parsing
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// var mysql = require('mysql');
-// // For jawsDB
-// var connection;
-// if (process.env.JAWSDB_URL) {
-//     // DB is JawsDB on Heroku
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     // DB is local on localhost
-//     connection = mysql.createConnection({
-//         port: 8080,
-//         host: 'localhost',
-//         user: 'root',
-//         password: '',
-//         database: 'projects_db'
-//     });
-// }
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-
+// Static directory
+app.use(express.static("public"));
 
 // Routes
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 // Syncing sequelize models and starting our Express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
